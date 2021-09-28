bb99664abc6027e16b68afee1e4c018e4650b309
judorsch <jdorsch@udel.edu>
2021-09-28 12:16:16 -0400

    get buttons working somewhat

--- src/Components/Results.tsx
+++ src/Components/Results.tsx
@@ -1,18 +1,22 @@
-import {Col, Container, Nav, Navbar} from 'react-bootstrap';
+import {Button, Col, Container, Nav, Navbar} from 'react-bootstrap';
 
 //Navbar came from https://react-bootstrap.github.io/components/navbar/
 export function Results(): JSX.Element{
+
+    function test(){
+        console.log("hi");
+    }
     return(
         <Col>
             <h2>Stock Results</h2>
             <Navbar bg="primary" variant="dark">
                 <Container>
                 <Nav className="me-auto">
-                    <Nav.Link href="#home">Today</Nav.Link>
-                    <Nav.Link href="#home">10 Days</Nav.Link>
-                    <Nav.Link href="#features">30 Days</Nav.Link>
-                    <Nav.Link href="#pricing">6 Months</Nav.Link>
-                    <Nav.Link href="#pricing">1 Year</Nav.Link>
+                    <Button onClick={test}>Today</Button>
+                    <Button onClick={test}>10 Days</Button>
+                    <Button onClick={test}>30 Days</Button>
+                    <Button onClick={test}>6 Months</Button>
+                    <Button onClick={test}>1 Year</Button>
                 </Nav>
                 </Container>
             </Navbar>
--- src/Components/Selector.tsx
+++ src/Components/Selector.tsx
@@ -1,10 +1,13 @@
 import {Button, Col} from 'react-bootstrap';
 
 export function Selector(): JSX.Element{
+    function test(){
+        console.log("Hi")
+    }
     return(
         <Col>
             <h2>Index Options</h2>
-            <Button>DOW</Button>
+            <Button onClick={test}>DOW</Button>
             <Button>S&P</Button>
             <Button>Nasdaq</Button>
             <Button>Russel</Button>
